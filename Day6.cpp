7b4da11e8659277a77fee23f4ce59d02800888ac
Maimoona <160620135+maimoona2024@users.noreply.github.com>
2024-04-10 22:54:41 +0500

    Add files via upload

---
+++ Day6.cpp
@@ -0,0 +1,57 @@
+//Program # 1: For finding square of a number.
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int x;
+//     cout<<"Please enter a number to find square: ";
+//     cin>>x;
+//     int square = x*x;
+//     cout<<"The square of "<<x<<" is "<<square<<".";
+// }
+
+//Program # 2: Typecasting.
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int x;
+//     cout<<"Please enter a number: ";
+//     cin>>x;
+//     float y = (float)x;
+//     cout<<y/2;
+// }
+
+
+//Program # 3: DMAS
+
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int x = 2+2*3/2-7;
+//     cout<<"The value of x is: "<<x;
+// }
+
+
+//Program # 4: char data type.
+
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     char a = '?';
+//     cout<<"The value of a is: "<<a<<".";
+// }
+
+//Program # 5: Program for ASCII values 
+
+#include<iostream>
+using namespace std;
+int main() {
+    char x;
+    cout<<"Please Enter a character: ";
+    cin>>x;
+    cout<<"The ASCII value of x is: "<<(int)x<<".";
+    }
+
